e4288fc04ed2eb57d6640bcc380dbe3078edc9cb
Justin Davila <isaac_justin@hotmail.com>
2026-08-28 20:50:58 -0600

    nueva tool con typescript, falta python

--- TypeScript/tool_usage.ts
+++ TypeScript/tool_usage.ts
@@ -17,11 +17,25 @@ const calculadora = tool({
   },
 });
 
+const sumar = tool({
+  name: "Sumar_tool",
+  description: "Suma n cantidad de números, ej: '1, 2, 3'",
+  inputSchema: z.object({
+    numeros: z.array(z.number()),
+  }),
+  callback: async ({ numeros }) => {
+    const resultado = numeros.reduce((acc, curr) => acc + curr, 0);
+    return `El resultado de la suma es ${resultado}`;
+  },
+})
+
 const agent = new Agent({
   model: "us.anthropic.claude-haiku-4-5-20251001-v1:0", // ajusta al model_id que tengas habilitado
-  systemPrompt: "Eres un asistente que ayuda con cálculos matemáticos. Usa la herramienta calculadora cuando sea necesario.",
-  tools: [calculadora],
+  systemPrompt: "Eres un asistente que ayuda con cálculos matemáticos. Usa las herramientas cuando sea necesario.",
+  tools: [calculadora, sumar],
 });
 
-const respuesta = await agent.invoke("¿Cuánto es 45 * 12 + 7?");
-console.log(respuesta);
+// const respuesta = await agent.invoke("¿Cuánto es 45 * 12 + 7?");
+const respuestaSuma = await agent.invoke("¿Cuál es el resultado de sumar 1, 2, 3, 4, 5 y así sucesivamente hasta llegar al 100?");
+// console.log(respuesta);
+console.log(respuestaSuma);
